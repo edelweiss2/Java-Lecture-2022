c55f5f3db89cfe290e56e6c53aa9c2a0ddf1e882
edelweiss2 <yunhokim2000@gmail.com>
2022-11-14 10:58:46 +0900

    chapter 15 questions

--- src/ch15_collection/Q09_HashMap.java
+++ src/ch15_collection/Q09_HashMap.java
@@ -2,6 +2,7 @@ package ch15_collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Q09_HashMap {
 
@@ -15,7 +16,19 @@ public class Q09_HashMap {
 		int maxScore = 0;
 		int totalScore = 0;
 		
-		map.
+		Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+		for(Map.Entry<String, Integer> entry : entrySet) {
+			if (entry.getValue() > maxScore) {
+				name = entry.getKey();
+				maxScore = entry.getValue();
+			}
+			totalScore += entry.getValue();			
+		}
+		
+		int avgScore = totalScore / map.size();
+		System.out.println("평균 점수: " + avgScore);
+		System.out.println("최고 점수: " + maxScore);
+		System.out.println("최고 점수를 받은 id: " + name);		
 		
 	}
 
