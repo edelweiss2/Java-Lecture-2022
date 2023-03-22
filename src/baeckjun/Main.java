5f01cfc7de958f384742eb056b7cf8c74df041e2
edelweiss2 <yunhokim2000@gmail.com>
2023-03-22 18:54:58 +0900

    백준

--- src/baeckjun/Main.java
+++ src/baeckjun/Main.java
@@ -1,41 +1,11 @@
 package baeckjun;
 
-public class Main {
-	
-	
-	public static void main(String[] args) {
-		int n = 5;
-		
-		int[] arrA = {1, 1, 1, 6, 0};
-		int[] arrB = {2, 7, 8, 3, 1};
-		
-		System.out.println(s(n,arrA, arrB));
+public class Main{
+	public static void main(String args[]){
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
 	}
-	
-	
-	public static int s(int n, int[] a, int[] b) {		
-		int s = 0;
-		
-		for(int i = 0; i < n; i++) {
-			s += a[i] * b[i];
-		}
-		
-		
-		return s;
-	}
-	
-	public int sMin(int n, int[] a, int[] b) {
-		int sMin = 0;
-		int bMax = b[0];
-		
-		for(int i= 0; i < n; i++) {
-			
-			sMin = a[i]*b[i];
-		}
-		
-		return sMin;
-	}
-
-	
 }
-
